9deee72b3318f5bc6333ca5a19a2525bf062558a
Atharva188 <atharva.ganpat18@gmail.com>
2024-01-06 13:48:46 +0530

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,18 @@
+const Footer = () => {
+    return (
+        <footer>
+            <div>
+                <h1>Webstar</h1>
+                <p>@all rights reserved</p>
+            </div>
+            <div>
+                <h5>Follow Us</h5>
+                <div>
+                    <a href="https://www.linkedin.com/in/atharva-ganpat-60559a223/">LinkedIn</a>
+                    <a href="https://github.com/Atharva188">GitHub</a>
+                </div>
+            </div>
+        </footer>
+    )
+}
+export default Footer;
